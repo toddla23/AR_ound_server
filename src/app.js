711e989ae3d4bfb0ad3a9c031a1015722a626c76
toddla23 <qkrwhdgur0319@gmail.com>
2025-11-16 23:42:33 +0900

    no message

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const {PORT} = require("./config.json")
 const storeRouter = require("./router/store.js")
+const discriptionRouter = require("./router/discription.js")
+
 
 app.get('/', (req, res) => {
     res.send('hello world');
 })
 app.use("/stores",storeRouter);
+
+app.use("/discriptions",discriptionRouter);
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
