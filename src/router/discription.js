4bc97cf53844d58a2447e9eb2b801a312d232160
toddla23 <qkrwhdgur0319@gmail.com>
2025-11-18 10:33:52 +0900

    [Fix] fix discription

--- src/router/discription.js
+++ src/router/discription.js
@@ -3,10 +3,7 @@ const stroeRepository = require("../repository/store");
 const router = express.Router();
 
 router.get("/1", async (req, res) => {
-  return res.send(`안녕하세요~ 저는 문경 약돌돼지예요!
-이 집의 고기는 특별해요. 문경의 특산물, 바로 약돌돼지를 쓰거든요!
-약돌을 먹여 키운 돼지라서 고기가 더 담백하고 맛있답니다. 가격도 최대한 착하게 책정하셨기 때문에, 많은 사람들이 부담 없이 즐길 수 있답니
-다!`);
+  return res.send(`안녕하세요~ 저는 문경 약돌돼지예요!이 집의 고기는 특별해요. 문경의 특산물, 바로 약돌돼지를 쓰거든요!약돌을 먹여 키운 돼지라서 고기가 더 담백하고 맛있답니다. 가격도 최대한 착하게 책정하셨기 때문에, 많은 사람들이 부담 없이 즐길 수 있답니다!`);
 });
 
 router.get("/2", async (req, res) => {
